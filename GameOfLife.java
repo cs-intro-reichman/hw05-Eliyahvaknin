35ae80b6e87289dcf25bb39b80995a66ea866470
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-22 22:37:41 +0200

    GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -9,12 +9,13 @@ public class GameOfLife {
 
 	public static void main(String[] args) {
 		String fileName = args[0];
+		String fileName = "glider.dat";
 		// Uncomment the test that you want to execute, and re-compile.
 		// (Run one test at a time).
 		// test1(fileName);
 		//  test2(fileName);
 		// test3(fileName, 3);
-		// play(fileName);
+		 play(fileName);
 	}
 	
 	// Reads the data file and prints the initial board.
@@ -205,7 +206,7 @@ public class GameOfLife {
 			}
 		}
 		StdDraw.show();
-		StdDraw.pause(100); 
+		StdDraw.pause(10); 
 	}
 }
 
